6c5f297b3db2114a72a620d15a6f9342560e9d7b
FabioMorrone <morronefabio04@gmail.com>
2025-02-26 14:04:29 +0100

    snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,15 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (let i = 0; i < names.length; i++) {
+    const thisName = names[i];
+    
+    console.log(thisName);
+    
+}
+
+names.forEach((name)=>{
+    console.log(name);
+    
+})
