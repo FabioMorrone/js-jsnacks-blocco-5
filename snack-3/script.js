06a9550a759b7e10389b1af2b6778ec591570016
FabioMorrone <morronefabio04@gmail.com>
2025-02-26 14:57:09 +0100

    snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,13 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbersPiu = []
+
+for (let i = 0; i < numbers.length; i++) {
+  const thisNumber = numbers[i];
+  numbersPiu.push(thisNumber + 1)
+
+}
+
+console.log(numbersPiu);
