251b119d5ccdb5170fccb7aaad942f33e05d7a12
FabioMorrone <morronefabio04@gmail.com>
2025-02-26 17:07:51 +0100

    add

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,24 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+let student = []
+
+for (let i = 0; i < students.length; i++) {
+  const thisStudents = students[i];
+  if (thisStudents.id === 2) {
+    student.push(thisStudents)
+    break
+  }
+}
+console.log(student);
+
+
+
+
+const studente = students.find((student) => {
+  return student.id === 2; 
+})
+
+console.log(studente);
